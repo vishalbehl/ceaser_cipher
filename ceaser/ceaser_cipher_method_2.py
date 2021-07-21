70c30e530715952747bd8c81aaa495bc725d04b9
Vishal Behl <57057146+vishalbehl@users.noreply.github.com>
2021-07-22 00:23:29 +0530

    Create ceaser_cipher_method_2.py

---
+++ ceaser/ceaser_cipher_method_2.py
@@ -0,0 +1,41 @@
+#Method 2
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+print(logo)
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+shift=shift%26          #If the shift amount is greater than alphabets
+
+def ceaser(cipher_direction,plain_text,shift_amount):
+    cipher_text=""
+    for letter in plain_text:
+        position=alphabet.index(letter)
+        if cipher_direction=="encode":
+            new_position=position+shift_amount
+            new_letter=alphabet[new_position]
+            cipher_text+=new_letter
+        elif cipher_direction=="decode":
+            new_position=position-shift_amount
+            new_letter=alphabet[new_position]
+            cipher_text+=new_letter
+    print(f"The {direction}d text is {cipher_text}")
+
+ceaser(cipher_direction=direction,plain_text=text,shift_amount=shift)
