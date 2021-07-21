f9b4ee0da40a1e24d0a6d6723d9cc8eda2d90ad9
Vishal Behl <57057146+vishalbehl@users.noreply.github.com>
2021-07-22 00:24:01 +0530

    Update and rename ceaser_cipher.py to ceaser_cipher_method_3.py

--- ceaser/ceaser_cipher_method_3.py
+++ ceaser/ceaser_cipher_method_3.py
@@ -16,61 +16,7 @@ a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8
               88           
 """
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
 print(logo)
-# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-# text = input("Type your message:\n").lower()
-# shift = int(input("Type the shift number:\n"))
-# shift=shift%26          #If the shift amount is greater than alphabets
-
-##############################################
-#Method: 1
-
-# def encrypt(plain_text,shift_amount):
-#     cipher_text=""
-#     for letter in plain_text:
-#         position=alphabet.index(letter)
-#         new_position=position+shift_amount
-#         new_letter=alphabet[new_position]
-#         cipher_text+=new_letter
-#     print(f"The encoded text is {cipher_text} ")
-
-# def decrypt(plain_text,shift_amount):
-#     cipher_text=""
-#     for letter in plain_text:
-#         position=alphabet.index(letter)
-#         new_position=position-shift_amount
-#         new_letter=alphabet[new_position]
-#         cipher_text+=new_letter
-#     print(f"The encoded text is {cipher_text} ")
-
-# if direction=="encode":
-#     encrypt(plain_text=text,shift_amount=shift)
-# elif direction=="decode":
-#     decrypt(plain_text=text,shift_amount=shift)
-
-###############################################
-#Method: 2
-
-# def ceaser(cipher_direction,plain_text,shift_amount):
-#     cipher_text=""
-#     for letter in plain_text:
-#         position=alphabet.index(letter)
-#         if cipher_direction=="encode":
-#             new_position=position+shift_amount
-#             new_letter=alphabet[new_position]
-#             cipher_text+=new_letter
-#         elif cipher_direction=="decode":
-#             new_position=position-shift_amount
-#             new_letter=alphabet[new_position]
-#             cipher_text+=new_letter
-#     print(f"The {direction}d text is {cipher_text}")
-
-# ceaser(cipher_direction=direction,plain_text=text,shift_amount=shift)
-
-###############################################
-#Method: 3
-
 def ceaser(cipher_direction,plain_text,shift_amount):
     cipher_text=""
     if cipher_direction=="decode":
